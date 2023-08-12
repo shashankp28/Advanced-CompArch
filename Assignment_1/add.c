58755ce0046358a287630c4ae31838586feca988
Tabish Khalid Halim <tkhalim14@gmail.com>
2023-08-12 12:52:00 -0700

    Commit Part-A
    Floating and Arithmetic Operands Done

--- Assignment_1/add.c
+++ Assignment_1/add.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 
+float add(float a, float b){
+    return a+b;
+}
+
 int main()
 {
-    int a=12389, b=9871, c, d, e, f;
-    c = a + b;
+    float a=12389.5, b=9871, c, d, e, f, g, h; 
+    c = add(a,b);
+    d = a - b;
     d = a - b;
     e = a * b;
     f = a / b;
+    g = f + a;
+    h = f + c;
     return 0;
 }
