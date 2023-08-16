439ba9d1709751d084e1d5db2b7b8d5b6743c0d4
Tabish Khalid Halim <tkhalim14@gmail.com>
2023-08-15 17:00:53 -0700

    Part-A Plots

--- Assignment_1/Part_A/graphit.py
+++ Assignment_1/Part_A/graphit.py
@@ -5,19 +5,29 @@ file_paths = ['1', '2', '3', '4', '5']
 
 # Load CSV files into DataFrames
 dataframes = [pd.read_csv('./Outputs/'+file_path, usecols=['Category', 'Percentage %']) for file_path in file_paths]
-fig, axs = plt.subplots(nrows=len(file_paths), ncols=1, figsize=( 3*len(file_paths), 2* len(file_paths)))
+# fig, axs = plt.subplots(nrows=len(file_paths), ncols=1, figsize=( 3*len(file_paths), 2* len(file_paths)))
+
 
 # Load CSV files into DataFrames and plot histograms
-for i, (ax, file_path) in enumerate(zip(axs, file_paths)):
+for i, ( file_path) in enumerate((file_paths)):
     df = pd.read_csv('./Outputs/'+file_path, usecols=['Category', 'Percentage %'])  # Skip the first column
-    
-    ax.bar(df['Category'], df['Percentage %'],width=1)
+    fig, ax = plt.subplots(nrows=1, ncols=1,figsize=( 2*len(file_paths), len(file_paths)*2))
+    pps = ax.bar(df['Category'], df['Percentage %'],width=0.5)
+    for p in pps:
+        height = p.get_height()
+        ax.annotate('{}'.format(height),
+            xy=(p.get_x() + p.get_width() / 2, height),
+            xytext=(0, 3), # 3 points vertical offset
+            textcoords="offset points",
+            ha='center', va='bottom')
+
     ax.set_title(f"Histogram for File {i+1}")
     ax.set_xlabel('Category')
     ax.set_ylabel('Percentage')
-    ax.tick_params(axis='x', labelsize=5)  # Rotate x-axis labels
+    ax.tick_params(axis='x', labelsize=10)  # Rotate x-axis labels
     ax.grid(True)
+    plt.tight_layout()
+    plt.show()
 
-plt.tight_layout()
-plt.show()
+
 
