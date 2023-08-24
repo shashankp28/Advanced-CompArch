e41c2f3101d035fc8ea7d1f76f4ad6aa03847a01
Shashank P <shashankp2832@gmail.com>
2023-08-24 14:52:50 +0530

    Added copy and run

---
+++ Assignment_2/shashankp_copy_run.sh
@@ -0,0 +1,7 @@
+version=pin-3.28-98749-g6643ecee5-gcc-linux
+current=$(pwd)
+cp ./inscount0.cpp ~/$version/source/tools/ManualExamples/inscount0.cpp
+cd ~/$version/source/tools/ManualExamples
+make
+../../../pin -t obj-intel64/inscount0.so -- $1 > $current/$2
+cd $current
