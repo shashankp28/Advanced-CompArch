8dfa9eecb9ae10bc8de85054f030b74306395199
Tabish Khalid Halim <tkhalim14@gmail.com>
2023-09-25 18:14:16 +0530

    updated plot file

--- Assignment_3/plot.py
+++ Assignment_3/plot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import json
 from itertools import product
 
@@ -38,8 +39,61 @@ size_maps = {
 file_names = [x[0]+x[1] for x in list(product(benchmarks, suffixes))]
 
 # print(file_names)
-for i,x in enumerate(size_maps[suffixes[1]]):
-    with open(base_directory+benchmarks[0]+suffixes[1]+str(x)+'.json', 'r') as json_file:
-        data_dict = json.load(json_file)
-        print(data_dict)
-    print(i)
+
+data_struct = {}
+
+# for benchmark_name in benchmarks:
+for s in suffixes:
+    benchData = {}
+    for benchmark_name in benchmarks:
+        grp_data = {}
+        for i,x in enumerate(size_maps[s]):
+            try:
+                with open(base_directory + benchmark_name + s + str(x)+'.json', 'r') as json_file:
+                    data_dict = json.load(json_file)
+                    # print(data_dict)
+            except:
+                data_dict = None
+            # print(i)
+            grp_data[str(x)]=data_dict
+        benchData[benchmark_name] = grp_data
+    data_struct[s] = benchData
+    
+
+# File has been created just use size_maps along with each entry in data_struct to get the plot.
+with open('file.json', 'w') as json_file:
+    json.dump(data_struct, json_file, indent=2)
+
+def generate_graph_plot(x,set_y, label_x, label_y):
+    # template for plotting
+
+    set_y = []
+    # Calculate y values for y = x and y = -x
+    set_y.append(x)
+    set_y.append(-x)
+
+    colors = ['green', 'red','blue','yellow','grey']
+    labels = benchmarks
+
+    # print(set_y)
+
+    for index, y in enumerate(set_y):
+        # Create the plot
+        # print(index)
+        plt.plot(x, y, color=colors[index], label=labels[index])
+
+    # Add labels and a legend
+    plt.xlabel(label_x)
+    plt.ylabel(label_y)
+    plt.legend()
+
+    # Add a title
+    plt.title('Plot of y = x and y = -x')
+
+    # Show the plot
+    plt.grid(True)
+    plt.axhline(0, color='black',linewidth=0.5)
+    plt.axvline(0, color='black',linewidth=0.5)
+    plt.show()
+
+generate_graph_plot(np.linspace(-10, 10, 100),[],'x','y')
