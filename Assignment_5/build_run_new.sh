967f7e4e3553802c103c54ef50335d83b9c7ebe6
Shashank P <shashankp2832@gmail.com>
2023-11-13 15:05:49 +0530

    Added new build and run

---
+++ Assignment_5/build_run_new.sh
@@ -0,0 +1,14 @@
+assignment_dir="/home/shashankp/Desktop/GitHub_S/Advanced-CompArch/Assignment_5"
+resource_dir="/home/shashankp/Desktop/GitHub_S/Advanced-CompArch/cs810_resources"
+
+cd $resource_dir/Tejas
+echo "Building Tejas..."
+ant make-jar
+echo "Successfully built Tejas"
+cd $assignment_dir
+
+
+java -jar $resource_dir/Tejas/jars/tejas.jar \
+    $assignment_dir/config_tigerlake.xml \
+    $assignment_dir/outputs/$1.stat \
+
