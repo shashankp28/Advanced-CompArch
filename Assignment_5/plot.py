8c1b8f2d98f2ffe7132ac4fe6fe0f987345c7157
Tabish Khalid Halim <tkhalim14@gmail.com>
2023-11-15 19:06:57 +0530

    atomic push

--- Assignment_5/plot.py
+++ Assignment_5/plot.py
@@ -5,10 +5,9 @@ import numpy as np
 # Specify the JSON file path
 json_file_path = './results.json'
 
-# Benchmarks and metrics
+# Benchmarks and prefetchers
 benchmarks = ['gcc', 'lbm', 'mcf', 'namd', 'xalancbmk', 'numenta-nab_8', 'gimp_8', 'blender_8', 'openssl_8']
-
-prefetchers = ['Simple','Power4','Bingo','None']
+prefetchers = ['Simple', 'Power4', 'Bingo', 'None']
 
 metrics = ["Micro Ops IPC", "I1 Hit Rate", "L1 Hit Rate", "L2 Hit Rate", "L3 Hit Rate"]
 
@@ -16,26 +15,30 @@ metrics = ["Micro Ops IPC", "I1 Hit Rate", "L1 Hit Rate", "L2 Hit Rate", "L3 Hit
 with open(json_file_path, 'r') as file:
     data = json.load(file)
 
-
-for benchmark in benchmarks:
-    # Prepare data for plotting
-
-    values = np.array([[data[benchmark][prefetcher][metric] for metric in metrics] for prefetcher in prefetchers])
-
-    # Plotting
+# Plotting
+for i, metric in enumerate(metrics):
     fig, ax = plt.subplots(figsize=(12, 9))
     bar_width = 0.2
-    index = np.arange(len(prefetchers)) * (bar_width+1) 
+    index = np.arange(len(benchmarks)) * (bar_width + 1)
 
-    for i, metric in enumerate(metrics):
-        ax.bar(index + i * bar_width, values[:, i], bar_width, label=metric)
+    ax.set_xlabel('Benchmarks')
+    if metric == 'Micro Ops IPC':
+        i=1
+        ax.set_ylabel('IPC')
+    else:
+        i=100
+        ax.set_ylabel('Percentage %')
+    ax.set_title(f'{metric} Across Benchmarks for Different Prefetchers')
 
-    ax.set_xlabel('Prefetchers')
-    ax.set_ylabel('Metric Values')
-    ax.set_title(f'Performance Metrics for {benchmark}')
-    ax.set_xticks(index + (len(metrics) - 1) * bar_width / 2)
-    ax.set_xticklabels(prefetchers)
+    ax.set_xticks(index + (len(prefetchers) - 1) * bar_width / 2)
+    ax.set_xticklabels(benchmarks)
+
+    for j, prefetcher in enumerate(prefetchers):
+        values = np.array([data[benchmark][prefetcher][metric]*i for benchmark in benchmarks])
+        ax.bar(index + j * bar_width, values, bar_width, label=prefetcher)
+    
     ax.legend()
 
     plt.tight_layout()
-    plt.savefig(f'./plots/{benchmark}.png')
+    plt.savefig(f'./plots/{metric}_across_benchmarks.png')
+    plt.close()  # Close the figure to avoid overlapping in the next iteration
